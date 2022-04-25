2b0ec72bc5445db063288217e69fdc9f5786f239
makcsholonik <makcsholonik@gmail.com>
2022-04-25 16:47:27 +0300

    Menu + dataMenu

---
+++ src/components/layout/sidebar/dataMenu.ts
@@ -0,0 +1,7 @@
+import * as Icons from '@mui/icons-material';
+
+export const menu = [
+	{ title : 'Моя страница', link : '/profile', icons : Icons.Home },
+	{ title : 'Друзья', link : '/friends', icons : Icons.People },
+	{ title : 'Новости', link : '/', icons : Icons.Article }
+];
